5279b2b8a673b1e9626b35d362e1d706dd7318ab
Vitor Salgado <vsalgadopb@gmail.com>
2019-08-05 20:52:45 -0400

    fix: add issue label

--- src/utils/github/index.js
+++ src/utils/github/index.js
@@ -4,7 +4,7 @@ const Github = require('octonode')
 const Config = require('../../config')
 const GithubApi = Github.client(Config.github.token)
 
-module.exports.addIssueLabel = pr =>
+module.exports.addIssueLabel = (hook, pr) =>
   GithubApi
-    .issue(Config.hooks.java.repo, pr.number)
+    .issue(hook.repository, pr.number)
     .addLabelsAsync(Config.hooks.java.labels)
